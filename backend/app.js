571c45c47b5b4a4abeb0be74307f22d8d4d0edbe
asim1909 <asimmohit123@gmail.com>
2025-03-03 23:52:07 +0530

    Update CORS configuration to allow Netlify domain

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,8 @@ app.use(cors({
     'http://localhost:3001',
     'http://localhost:5173',
     'https://hospital-finder.netlify.app',
-    'https://hospital-finder-asim.netlify.app'
+    'https://hospital-finder-asim.netlify.app',
+    'https://precious-shortbread-fc5616.netlify.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
